8df4349bb1aa6f3559c328e8a166798cd9b97919
Eline Ledent <16385013+ElineLedent@users.noreply.github.com>
2019-04-23 16:28:17 +0200

    Added a menu manager that tracks if a menu is already showing. Added some simple placemen logic to the about button script to have it at least show up somewhere in front of the camera. Added that loss of tracking hides menu

--- Apps/GalaxyExplorerV2_Unity/Assets/scripts/HandMenu.cs
+++ Apps/GalaxyExplorerV2_Unity/Assets/scripts/HandMenu.cs
@@ -8,85 +8,58 @@ public class HandMenu : MonoBehaviour
     [SerializeField]
     private GameObject _menuParent;
 
-    private Transform _cameraTransform;
-
     [SerializeField]
     private float _minShowingAngle = 130f;
 
+    private AttachToControllerSolver _attachToControllerSolver;
+    private HandMenuManager _handMenuManager;
     private float _currentAngle = 0f;
+    private Transform _cameraTransform;
 
-    private bool _menuIsShowing = false;
-
-    private bool _isTracking = false;
-
-    //private bool _isTrackingLeft = false;
-    //private bool _isTrackingRight = false;
+    public bool MenuIsShowingOnThisHand { get; private set; } = false;
 
     private void Start()
     {
         ShowHideMenu(false);
+
+        _handMenuManager = FindObjectOfType<HandMenuManager>();
+
+        _attachToControllerSolver = GetComponent<AttachToControllerSolver>();
+        _attachToControllerSolver.TrackingLost += OnTrackingLost;
+
         _cameraTransform = Camera.main.transform;
-
-        // TODO: Add tracking events callbacks
-        // TODO: Determine which controller tracker this script is linked to and only listen to those tracking events
-        // TODO: Implement that only one menu can show at the same time
-        // TODO: Also implement button presses (including affordances!)
-        // TODO: Re-check David's and Dan's video
-
-        //_controllerTransformTracker.LeftTrackingStarted += OnLeftTrackingStarted;
-        //_controllerTransformTracker.RightTrackingStarted += OnRightTrackingStarted;
     }
-
-    //private void OnLeftTrackingStarted()
-    //{
-    //    _isTrackingLeft = true;
-    //}
-
-    //private void OnRightTrackingStarted()
-    //{
-    //    _isTrackingRight = true;
-    //}
-
-    //private void OnLeftTrackingLost()
-    //{
-    //    _isTrackingLeft = false;
-
-    //    if (_menuIsShowing)
-    //    {
-    //        ShowHideMenu(false);
-    //    }
-    //}
-
-    //private void OnRightTrackingLost()
-    //{
-    //    _isTrackingRight = false;
-
-    //    if (_menuIsShowing)
-    //    {
-    //        ShowHideMenu(false);
-    //    }
-    //}
 
     private void Update()
     {
-        //if (!_isTracking) { return; }
-
-        _currentAngle = CalculateAngle();
-
-        if (_currentAngle > _minShowingAngle && !_menuIsShowing)
+        if (_attachToControllerSolver.IsTracking)
         {
-            ShowHideMenu(true);
+            _currentAngle = CalculateAngle();
+
+            if (_currentAngle > _minShowingAngle && !MenuIsShowingOnThisHand)
+            {
+                // Check if the menu is already showing on the other hand
+                if (!_handMenuManager.MenuIsAlreadyShowing)
+                {
+                    ShowHideMenu(true);
+                }
+            }
+            else if (_currentAngle < _minShowingAngle && MenuIsShowingOnThisHand)
+            {
+                ShowHideMenu(false);
+            }
         }
-        else if (_currentAngle < _minShowingAngle && _menuIsShowing)
-        {
-            ShowHideMenu(false);
-        }
+    }
+
+    private void OnTrackingLost()
+    {
+        ShowHideMenu(false);
     }
 
     private void ShowHideMenu(bool show)
     {
         _menuParent.SetActive(show);
-        _menuIsShowing = show;
+        MenuIsShowingOnThisHand = show;
     }
 
     private float CalculateAngle()
@@ -95,9 +68,6 @@ public class HandMenu : MonoBehaviour
 
         float angle = Mathf.Acos(angleCos);
         angle = angle * Mathf.Rad2Deg;
-
-        Debug.Log("Angle cosine = " + angleCos.ToString());
-        Debug.Log("Current angle = " + angle.ToString());
 
         return angle;
     }
