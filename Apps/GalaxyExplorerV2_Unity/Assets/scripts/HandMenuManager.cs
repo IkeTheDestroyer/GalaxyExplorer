80d7cca29d8fead451100530e06343690e71947c
Eline Ledent <16385013+ElineLedent@users.noreply.github.com>
2019-04-25 16:44:14 +0200

    [14677] Added spatial audio to the hand menu.

--- Apps/GalaxyExplorerV2_Unity/Assets/scripts/HandMenuManager.cs
+++ Apps/GalaxyExplorerV2_Unity/Assets/scripts/HandMenuManager.cs
@@ -8,8 +8,32 @@ public class HandMenuManager : MonoBehaviour
     [SerializeField]
     private HandMenu _handMenuRight;
 
+    [SerializeField]
+    private AudioSource _movableAudioSource;
+
+    [SerializeField]
+    private AudioClip _menuAppearAudioClip;
+
+    [SerializeField]
+    private AudioClip _menuDisappearAudioClip;
+
     public bool IsAMenuVisible
     {
         get { return _handMenuLeft.IsVisible || _handMenuRight.IsVisible; }
+    }
+
+    public void PlayMenuAudio(Vector3 position, bool isAppearing)
+    {
+        if (isAppearing)
+        {
+            _movableAudioSource.clip = _menuAppearAudioClip;
+        }
+        else
+        {
+            _movableAudioSource.clip = _menuDisappearAudioClip;
+        }
+
+        _movableAudioSource.transform.position = position;
+        _movableAudioSource.Play();
     }
 }
