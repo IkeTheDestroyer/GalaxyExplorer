f3d0d5eabedca8b76b3ed389fc5fadb8c577133e
Eline Ledent <16385013+ElineLedent@users.noreply.github.com>
2019-05-23 16:44:14 +0200

    [15093] Adde fade in/out to POI highlighting

--- Apps/GalaxyExplorerV2_Unity/Assets/scripts/POIHighlight.cs
+++ Apps/GalaxyExplorerV2_Unity/Assets/scripts/POIHighlight.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 public class POIHighlight : MonoBehaviour
 {
     [SerializeField]
-    private float _startOffset = -1f;
+    private float _startOffset = 1f;
 
     [SerializeField]
-    private float _endOffset = 1f;
+    private float _endOffset = -1f;
 
     [SerializeField]
-    private float duration = 1f;
+    private float _totalDuration = 1f;
 
     [SerializeField]
     private float _delay = 0f;
+
+    private float _fadeInEndPercentage = 0.2f;
+    private float _fadeOutStartPercentage = 0.5f;
 
     private Material _material;
 
@@ -27,28 +30,76 @@ public class POIHighlight : MonoBehaviour
         StartLerpTextureOffset();
     }
 
-    public void StartLerpTextureOffset()
+    private void OnDisable()
     {
-        StartCoroutine(LerpTextureOffset(duration));
     }
 
-    private IEnumerator LerpTextureOffset(float duration)
+    private void OnDestroy()
     {
-        _material.SetTextureOffset("_MainTex", new Vector2(_startOffset, 0));
+        SetAlphaOnMaterial(1f);
+        _material.SetTextureOffset("_MainTex", Vector2.zero);
+    }
 
-        Vector2 newOffset = _material.GetTextureOffset("_MainTex");
+    public void StartLerpTextureOffset()
+    {
+        StartCoroutine(LerpTextureOffset());
+    }
+
+    private IEnumerator LerpTextureOffset()
+    {
+        // Reset all values
+        Vector2 newOffset = new Vector2(_endOffset, 0);
+        _material.SetTextureOffset("_MainTex", newOffset);
+
+        float alpha = 0f;
+        SetAlphaOnMaterial(alpha);
+
+        float timer = 0f;
 
         yield return new WaitForSeconds(_delay);
 
-        while (duration > 0)
+        while (timer < _totalDuration)
         {
-            newOffset.x = Mathf.Lerp(_startOffset, _endOffset, duration);
+            // Normalize the current time passed
+            float currentPercentage = Mathf.Lerp(0f, 1f, timer / _totalDuration);
 
+            // Set the texture offset
+            newOffset.x = Mathf.Lerp(_startOffset, _endOffset, currentPercentage);
             _material.SetTextureOffset("_MainTex", newOffset);
 
-            duration -= Time.deltaTime;
+            // Set the alpha on the material
+            alpha = GetNewAlpha(timer, currentPercentage);
+            SetAlphaOnMaterial(alpha);
+            Debug.Log("Current alpha = " + alpha);
+
+            timer += Time.deltaTime;
 
             yield return null;
         }
+    }
+
+    private float GetNewAlpha(float timer, float currentPercentage)
+    {
+        float alpha = 0f;
+
+        if (currentPercentage < _fadeInEndPercentage)
+        {
+            alpha = Mathf.Lerp(0f, 1f, currentPercentage / _fadeInEndPercentage);
+        }
+        else if (currentPercentage < _fadeOutStartPercentage)
+        {
+            alpha = 1;
+        }
+        else
+        {
+            alpha = Mathf.Lerp(1f, 0f, ((currentPercentage - _fadeOutStartPercentage) / (1f - _fadeOutStartPercentage)) * 1f / _fadeOutStartPercentage);
+        }
+
+        return alpha;
+    }
+
+    private void SetAlphaOnMaterial(float alpha)
+    {
+        _material.color = new Color(_material.color.r, _material.color.b, _material.color.g, alpha);
     }
 }
