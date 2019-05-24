baeb1db9a2c6d1018d94966e31d438db8dc1aa27
Eline Ledent <16385013+ElineLedent@users.noreply.github.com>
2019-05-24 09:58:53 +0200

    [15093] Extra highlights have been added to the large POI's of the Solar System and Galactic Center

--- Apps/GalaxyExplorerV2_Unity/Assets/scripts/POIHighlight.cs
+++ Apps/GalaxyExplorerV2_Unity/Assets/scripts/POIHighlight.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 public class POIHighlight : MonoBehaviour
 {
     [SerializeField]
-    private float _startOffset = 1f;
-
-    [SerializeField]
-    private float _endOffset = -1f;
-
-    [SerializeField]
     private float _totalDuration = 1f;
 
     [SerializeField]
     private float _delay = 0f;
 
+    private float _startOffset = -1f;
+    private float _endOffset = 1f;
+    private float _textureOffsetY = 0f;
+
     private float _fadeInEndPercentage = 0.2f;
     private float _fadeOutStartPercentage = 0.5f;
 
     private Material _material;
+
+    private void Awake()
+    {
+        _material = GetComponent<Renderer>().material;
+        Vector2 textureOffset = _material.GetTextureOffset("_MainTex");
+        _textureOffsetY = textureOffset.y;
+    }
 
     private void OnEnable()
     {
@@ -48,7 +53,7 @@ public class POIHighlight : MonoBehaviour
     private IEnumerator LerpTextureOffset()
     {
         // Reset all values
-        Vector2 newOffset = new Vector2(_endOffset, 0);
+        Vector2 newOffset = new Vector2(_startOffset, _textureOffsetY);
         _material.SetTextureOffset("_MainTex", newOffset);
 
         float alpha = 0f;
@@ -64,7 +69,7 @@ public class POIHighlight : MonoBehaviour
             float currentPercentage = Mathf.Lerp(0f, 1f, timer / _totalDuration);
 
             // Set the texture offset
-            newOffset.x = Mathf.Lerp(_startOffset, _endOffset, currentPercentage);
+            newOffset.x = Mathf.Lerp(_endOffset, _startOffset, currentPercentage);
             _material.SetTextureOffset("_MainTex", newOffset);
 
             // Set the alpha on the material
