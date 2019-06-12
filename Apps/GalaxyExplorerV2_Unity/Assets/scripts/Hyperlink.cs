6b69e31e24f38a7a291ae7d15b35edb8a765a42a
Eline Ledent <16385013+ElineLedent@users.noreply.github.com>
2019-06-12 15:40:35 +0200

    [14897] Added about slate prefab. Reworking existing hyperlink script to add the hyperlinks.

--- Apps/GalaxyExplorerV2_Unity/Assets/scripts/Hyperlink.cs
+++ Apps/GalaxyExplorerV2_Unity/Assets/scripts/Hyperlink.cs
@@ -1,77 +1,83 @@
-﻿//// Copyright Microsoft Corporation. All rights reserved.
-//// Licensed under the MIT License. See LICENSE in the project root for license information.
-//
-////using HoloToolkit.Unity.InputModule;
-//using System;
-//using UnityEngine;
-//
-//namespace GalaxyExplorer
-//{
-//    public class Hyperlink : MonoBehaviour, IInputClickHandler, IControllerTouchpadHandler
-//    {
-//        public string URL;
-//
-//        public event Action Clicked;
-//
-//        void OnMouseDown()
-//        {
-//            OnInputUp(null);
-//        }
-//
-//        public void OnHoldCanceled()
-//        {
-//        }
-//
-//        public void OnHoldCompleted()
-//        {
-//            OnInputUp(null);
-//        }
-//
-//        public void OnHoldStarted()
-//        {
-//        }
-//
-//        public void OnInputPositionChanged(InputPositionEventData eventData)
-//        {
-// 
-//        }
-//
-//        public void OnInputUp(InputEventData eventData)
-//        {
-//            
-//        }
-//
-//        public void OnTouchpadReleased(InputEventData eventData)
-//        {
-//            OnInputClicked(null);
-//
-//            eventData.Use();
-//        }
-//
-//        public void OnTouchpadTouched(InputEventData eventData)
-//        {
-//
-//        }
-//
-//        public void OnInputClicked(InputClickedEventData eventData)
-//        {
-//            if (Clicked != null)
-//            {
-//                Clicked();
-//            }
-//
-//            if (!string.IsNullOrEmpty(URL))
-//            {
-//#if NETFX_CORE
-//                UnityEngine.WSA.Application.InvokeOnUIThread(() =>
-//                {
-//                    var uri = new System.Uri(URL);
-//                    var unused = Windows.System.Launcher.LaunchUriAsync(uri);
-//                }, false);
-//#else
-//                Application.OpenURL(URL);
-//#endif
-//            }
-//        }
-//    }
-//}
+﻿// Copyright Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Input;
+
+using System;
+using UnityEngine;
+
+namespace GalaxyExplorer
+{
+    public class Hyperlink : MonoBehaviour, IMixedRealityInputHandler
+    {
+        public string URL;
+
+        public event Action Clicked;
+
+        public void OnInputDown(InputEventData eventData)
+        {
+            Debug.Log("Link clicked: " + URL.ToString());
+
+            if (Clicked != null)
+            {
+                Clicked();
+            }
+
+            if (!string.IsNullOrEmpty(URL))
+            {
+                //#if NETFX_CORE
+                //                            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+                //                            {
+                //                                var uri = new System.Uri(URL);
+                //                                var unused = Windows.System.Launcher.LaunchUriAsync(uri);
+                //                            }, false);
+                //#else
+                Application.OpenURL(URL);
+                //#endif
+            }
+        }
+
+        public void OnInputUp(InputEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        //private void OnMouseDown()
+        //{
+        //    OnInputUp(null);
+        //}
+
+        //public void OnHoldCompleted()
+        //{
+        //    OnInputUp(null);
+        //}
+
+        //public void OnTouchpadReleased(InputEventData eventData)
+        //{
+        //    OnInputClicked(null);
+
+        //    eventData.Use();
+        //}
+
+        //        public void OnInputClicked(InputClickedEventData eventData)
+        //        {
+        //            if (Clicked != null)
+        //            {
+        //                Clicked();
+        //            }
+
+        //            if (!string.IsNullOrEmpty(URL))
+        //            {
+        //#if NETFX_CORE
+        //                UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+        //                {
+        //                    var uri = new System.Uri(URL);
+        //                    var unused = Windows.System.Launcher.LaunchUriAsync(uri);
+        //                }, false);
+        //#else
+        //                Application.OpenURL(URL);
+        //#endif
+        //            }
+        //        }
+    }
+}
