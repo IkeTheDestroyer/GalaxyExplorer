6792436b635e6af3e9e9faec5cc397d5da4ab3be
Ben Turner <ben.turner@microsoft.com>
2019-06-12 17:15:33 +0200

    [15378] Move light when looking at planets

--- Apps/GalaxyExplorerV2_Unity/Assets/scripts/PlanetPreviewController.cs
+++ Apps/GalaxyExplorerV2_Unity/Assets/scripts/PlanetPreviewController.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlanetPreviewController : MonoBehaviour
 {
     [SerializeField] private Button[] buttons;
+    [SerializeField] private Vector3 lightDestinationPosition;
+
+    private GameObject lightObject;
+    private Vector3 lightInitialPosition;
+    private bool movingLightToDestination;
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += HandleSceneLoaded;
+    }
 
     public void OnButtonSelected(Button selectedButton)
     {
@@ -13,5 +23,49 @@ public class PlanetPreviewController : MonoBehaviour
         {
             button.interactable = button != selectedButton;
         }
+
+        if (lightObject == null)
+        {
+            lightObject = GameObject.Find("LightSourcePosition");
+            if (lightObject != null)
+            {
+                lightInitialPosition = lightObject.transform.position;
+            }
+        }
+
+        if (selectedButton != null)
+        {
+            if (!movingLightToDestination)
+            {
+                StartCoroutine(MoveLight(true));
+            }
+        }
+        else
+        {
+            StartCoroutine(MoveLight(false));
+        }
+    }
+
+    private void HandleSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        lightObject = null;
+    }
+
+    private IEnumerator MoveLight(bool toDestination)
+    {
+        if (lightObject != null)
+        {
+            if (toDestination)
+            {
+                lightObject.transform.position = lightDestinationPosition;
+                movingLightToDestination = true;
+            }
+            else
+            {
+                lightObject.transform.position = lightInitialPosition;
+                movingLightToDestination = false;
+            }
+        }
+        yield return null;
     }
 }
